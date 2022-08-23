21e7c24d023614ee99f2a86702d2c2ac08ad2dcd
ignxal <ignacio.logiudice@gmail.com>
2022-08-23 19:25:41 -0300

    add exercise 2

--- Ejercicio_02/Program.cs
+++ Ejercicio_02/Program.cs
@@ -4,9 +4,27 @@ namespace Ejercicio_02
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string numInput;
+            float parsedNum;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numInput = Console.ReadLine();
+
+            while (float.TryParse(numInput, out parsedNum) == false || parsedNum <= 0)
+            {
+                Console.WriteLine("\n\nERROR. ¡Reingresar número!\n\n");
+                Console.WriteLine("Ingrese un numero: ");
+                numInput = Console.ReadLine();
+            }
+   
+            double squaredNum = Math.Pow(parsedNum, 2);
+            double cubedNum = Math.Pow(parsedNum, 3);
+
+            Console.WriteLine($"Numero al cuadrado: {squaredNum}");
+            Console.WriteLine($"Numero al cuadrado: {cubedNum}");
+
         }
     }
 }
