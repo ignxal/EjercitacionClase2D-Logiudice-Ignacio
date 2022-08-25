85514133fa94f8d2948dcd63b340080ac0b16ac1
ignxal <ignacio.logiudice@gmail.com>
2022-08-24 21:55:40 -0300

    add exercise 4

---
+++ Ejercicio_04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numFromIteration = 0;
+            int totalPerfectNumFound = 0;
+
+            Console.WriteLine("\nPrimeros 4 numeros perfectos:");
+            while (totalPerfectNumFound < 4)
+            {
+                if (IsPerfectNum(numFromIteration))
+                {
+                    Console.Write(numFromIteration + " - ");
+                    totalPerfectNumFound++;
+                }
+                numFromIteration++;
+            }
+
+
+        }
+
+        static bool IsPerfectNum(int number)
+        {
+            bool returnValue = false;
+            int dividers = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i != 0)
+                {
+                    continue;
+                }
+
+                dividers += i;
+            }
+
+            if(number - dividers == 0)
+            {
+                returnValue = true;
+            }
+
+            return returnValue;
+        }
+    }
+}
