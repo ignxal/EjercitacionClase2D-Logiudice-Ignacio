7027d298eabf9e0ba278b7126e04bfa3aef13624
ignxal <ignacio.logiudice@gmail.com>
2022-08-26 23:02:40 -0300

    add exercise 2-2

---
+++ 2_Ejercicio_02/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _2_Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            int parsedNumber;
+            int totalSum = 0;
+            string userAnswer;
+
+            do
+            {
+                parsedNumber = GetInt("Ingrese un numero: ", "ERROR. ¡Reingresar numero!");
+                totalSum += parsedNumber;
+
+                userAnswer = GetAnswer("¿Desea continuar? (S/N)", "ERROR. ¡Reingresar respuesta!");
+
+            } while (Validator.ValidateAnswer(userAnswer));
+
+            DisplayResults(totalSum);
+        }
+
+        static int GetInt(string consoleMessage, string consoleMessageError)
+        {
+            string userInput;
+            bool inputWasParsed;
+
+            userInput = GetUserInput(consoleMessage);
+            inputWasParsed = int.TryParse(userInput, out int parsedInt);
+
+            while (!inputWasParsed)
+            {
+                userInput = GetUserInput(consoleMessage, consoleMessageError);
+                inputWasParsed = int.TryParse(userInput, out parsedInt);
+            }
+
+            return parsedInt;
+        }
+
+        static string GetUserInput(string consoleMessage, string consoleMessageError = "false")
+        {
+            if (consoleMessageError != "false")
+            {
+                Console.WriteLine($"\n{consoleMessageError}\n");
+            }
+            Console.Write($"{consoleMessage}");
+
+            return Console.ReadLine();
+        }
+
+        static string GetAnswer(string consoleMessage, string consoleMessageError)
+        {
+            string userInput;
+            userInput = GetUserInput(consoleMessage);
+
+            while (userInput != "S" && userInput != "N")
+            {
+                userInput = GetUserInput(consoleMessage, consoleMessageError);
+            }
+
+            return userInput;
+        }
+
+        static void DisplayResults(int number)
+        {
+            Console.WriteLine("\n\n##### Resultado #####\n");
+            Console.WriteLine($"Suma total: {number}");
+        }
+    }
+}
---
+++ 2_Ejercicio_02/Validator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Ejercicio_02
+{
+    static class Validator
+    {
+        public static bool ValidateAnswer(string answer)
+        {
+            bool returnValue = true;
+
+            if (answer != "S")
+            {
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+    }
+}
