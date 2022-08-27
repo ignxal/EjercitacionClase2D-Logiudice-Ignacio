13c40fe0c468f3f1566d64fddecf02da37b85343
ignxal <ignacio.logiudice@gmail.com>
2022-08-27 00:02:15 -0300

    add exercise 2-3

---
+++ 2_Ejercicio_03/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Ejercicio_03
+{
+    static class Converter
+    {
+        public static string ConvertDecimalToBinary(int number)
+        {
+            string result = string.Empty;
+            int rem;
+
+            while (number > 0 || string.IsNullOrEmpty(result))
+            {
+                rem = number % 2;
+                number /= 2;
+                result = rem.ToString() + result;
+            }
+
+            return result;
+        }
+
+        public static string ConvertBinaryToDecimal(int number)
+        {
+            int baseVal = 1, rem;
+            int result = 0;
+
+
+            while (number > 0)
+            {
+                rem = number % 10;
+                result += rem * baseVal;
+                number /= 10;
+                baseVal *= 2;
+            }
+
+            return result.ToString();
+        }
+
+    }
+}
---
+++ 2_Ejercicio_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2_Ejercicio_03
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = 20;
+            string numberInBinary;
+            int binary = 010101;
+            string binaryInDecimal;
+
+            numberInBinary = Converter.ConvertDecimalToBinary(number);
+            binaryInDecimal = Converter.ConvertBinaryToDecimal(binary);
+
+            DisplayResults(number, numberInBinary, binary, binaryInDecimal);
+        }
+
+        static void DisplayResults(int number, string numberInBinary, int binary, string binaryInDecimal)
+        {
+            Console.WriteLine("\n\n##### Resultados #####\n");
+            Console.WriteLine($"Numero original: {number}");
+            Console.WriteLine($"Numero en binario: {numberInBinary}");
+
+            Console.WriteLine($"Binario original: {binary}");
+            Console.WriteLine($"Binario en decimal: {binaryInDecimal}");
+        }
+    }
+}
